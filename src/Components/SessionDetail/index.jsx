54e118c2cd73f034649177b87e6e7618a4ba6693
Amash10 <amashansari74@gmail.com>
2024-07-27 17:04:57 +0530

    session details

--- src/Components/SessionDetail/index.jsx
+++ src/Components/SessionDetail/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { sessionDetail } from "../../ReduxManager/action";
+import ElectricBoltOutlinedIcon from "@mui/icons-material/ElectricBoltOutlined";
+import ActiveImg from "../../Assets/SVG/status-updated-svgrepo-com.svg";
+import InactiveImg from "../../Assets/SVG/status-failed-svgrepo-com.svg";
+import databaseLogo from "../../Assets/SVG/database.svg";
 
 const CustomSessionsDetail = () => {
-  const sessiondetail = useSelector((state) => state.sessDetail)
-  const dispatch = useDispatch()
+  const sessiondetail = useSelector((state) => state.sessDetail);
+  const dispatch = useDispatch();
 
   const handleSessionToggle = () => {
-    dispatch(sessionDetail(sessiondetail))
-  }
+    dispatch(sessionDetail(sessiondetail));
+  };
   return (
     <>
       <div className="main-session-detail">
@@ -55,9 +59,66 @@ const CustomSessionsDetail = () => {
           </div>
           <div className="session-performance">
             <p className="mb-0 my-2">Session Performance</p>
-            <div className="session-performance-active performance rounded-1"></div>
-            <div className="session-performance-agent performance rounded-1"></div>
-            <div className="session-performance-database performance rounded-1"></div>
+            <div className="session-performance-active performance px-1 py-2 ">
+              <div className="spa-top d-flex justify-content-between">
+                <div className="session-num">
+                  <ElectricBoltOutlinedIcon></ElectricBoltOutlinedIcon>
+                  Session Active
+                </div>
+                <div className="session-active-since">
+                  Session Active: 12:05:33
+                </div>
+              </div>
+              <div className="session-state">Session-state : DEV</div>
+              <div className="sse d-flex justify-content-between">
+                <div className="session-energy">
+                  Power Consumption: Moderate
+                </div>
+                <div className="session-share-btn text-decoration-underline">
+                  Share Session
+                </div>
+              </div>
+            </div>
+            <div className="session-performance-agent performance px-1 py-2">
+              <div className="agent-name">Agent used: Text To Image</div>
+              <div className="agent-performance-consumption d-flex justify-content-between">
+                <div className="agent-performance">Agent Performance: Good</div>
+                <div className="agent-consumption">Power : Moderate</div>
+              </div>
+              <div className="agent-active-mute">
+                <div className="doc-index-right d-flex justify-content-between mt-2">
+                  <div className="actie-mute-sess ">
+                    <span className=" activeIndex activeMuteIndex m-0 me-2 ">
+                      Active <img src={ActiveImg} alt="" />
+                    </span>
+                    <span className=" activeMuteIndex">
+                      Mute <img src={InactiveImg} alt="" />
+                    </span>
+                  </div>
+                  <div className="session-active-since">
+                    Agent Active: 05:29:43
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="session-performance-database performance px-1 py-2">
+              <div className="database-name d-flex">
+                <div className="index-logo">
+                  <img src={databaseLogo} alt="" />
+                </div>
+                <span>Database Performance for Active session </span>
+              </div>
+              <div className="database-consumption">
+                Database Consumption: High
+              </div>
+              <div className="database-size">Database Size: 2.2GB</div>
+              <div className="database-connect-disconnect d-flex justify-content-between">
+                <div className="database-connect">
+                  Connect to new database +
+                </div>
+                <div className="database-disconect">Disconnect -</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
